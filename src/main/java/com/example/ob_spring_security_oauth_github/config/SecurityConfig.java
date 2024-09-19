fb72e769b1a5afe0362e999fd26124e48955f77c
“Flavia <flaviferri90@gmail.com>
2024-09-19 12:51:27 +0200

    login completed

--- src/main/java/com/example/ob_spring_security_oauth_github/config/SecurityConfig.java
+++ src/main/java/com/example/ob_spring_security_oauth_github/config/SecurityConfig.java
@@ -1,10 +1,10 @@
 package com.example.ob_spring_security_oauth_github.config;
 
 
-import com.example.ob_spring_security_oauth_github.service.UserOAuth2UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,23 +13,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    UserOAuth2UserService userOAuth2UserService;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http
                 .authorizeHttpRequests((authz) -> authz
                         .requestMatchers("/", "/login**").permitAll()
                         .anyRequest().authenticated()
                 )
-         /*      .oauth2Login(Customizer.withDefaults());
-        return http.build();*/
-
-                .oauth2Login(oauth2 -> oauth2
-                        .userInfoEndpoint(userInfo -> userInfo
-                                .userService(userOAuth2UserService)
-                        )
-                );
+           .oauth2Login(Customizer.withDefaults());
         return http.build();
+
+
     }
     }
