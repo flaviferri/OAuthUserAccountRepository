e8250b6dea247dbad547f2c24883d7e764365059
“Flavia <flaviferri90@gmail.com>
2024-09-19 16:39:22 +0200

    loginworking

---
+++ src/main/java/com/example/ob_spring_security_oauth_github/config/model/User.java
@@ -0,0 +1,24 @@
+package com.example.ob_spring_security_oauth_github.config.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String githubId;
+    @Column(nullable = false)
+    private String login;
+    private String name;
+    private String email;
+    private String avatarUrl;
+
+}
